7311f21808d79977ba0b04d235625b90c696abb6
resuldagdanov <resuldagdanov@gmail.com>
2021-11-30 18:22:20 +0300

    detailed readme is added

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # carla-imitation-learning
 Imitation Learning Model Training in Carla with DAgger
+
+## Contents
+1. [Setup](#setup)
+2. [Configuration](#configuration)
+2. [Running Carla Server](#running-carla-server)
+4. [Run Autopilot](#run-autopilot)
+5. [Training](#training)
+6. [Evaluation](#evaluation)
+
+## Setup
+Clone the repo and build the environment
+
+```Shell
+git clone https://github.com/resuldagdanov/carla-imitation-learning.git
+cd carla
+conda create -n carla python=3.7
+pip3 install -r requirements.txt
+conda activate carla
+```
+
+Download and setup CARLA 0.9.13
+```Shell
+wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.13.tar.gz
+wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/AdditionalMaps_0.9.13.tar.gz
+tar -xf CARLA_0.9.13.tar.gz
+tar -xf AdditionalMaps_0.9.13.tar.gz
+rm CARLA_0.9.13.tar.gz
+rm AdditionalMaps_0.9.13.tar.gz
+```
+
+## Configuration
+Open bashrc and include CARLA_ROOT, LEADERBOARD_ROOT, and SCENARIO_RUNNER_ROOT
+```Shell
+export CARLA_ROOT=~/carla
+export SCENARIO_RUNNER_ROOT=~/carla-imitation-learning/scenario_runner
+export LEADERBOARD_ROOT=~/carla-imitation-learning/leaderboard
+```
+
+## Running Carla Server
+Open terminal in the folder where Carla 0.9.13 is installed.
+```Shell
+./CarlaUE4.sh --world-port=2000 --resx=300 --resy=400 --quality-level=Epic -vulkan
+```
+
+## Run Autopilot
+Configure ```./scripts/run_autopilot.sh``` file !
+```Shell
+./scripts/run_autopilot.sh
+```
+
+## Training
+All training files are in ```./imitation_agents/trainings/``` and ```./imitation_agents/networks/```
+Models are saved inside ```./checkpoints/models/``` file.
+
+## Evaluation
+Spin up a CARLA server (described above).
+```Shell
+./scripts/leaderboard_evaluation.sh
+```
--- scripts/run_autopilot.sh
+++ scripts/run_autopilot.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export CARLA_ROOT=carla
 export CARLA_SERVER=${CARLA_ROOT}/CarlaUE4.sh
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
 export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
@@ -8,9 +7,7 @@ export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.10-py3.
 export PYTHONPATH=$PYTHONPATH:leaderboard
 export PYTHONPATH=$PYTHONPATH:scenario_runner
 
-export LEADERBOARD_ROOT=leaderboard
 export CHALLENGE_TRACK_CODENAME=SENSORS
-export TEAM_CONFIG=checkpoints/models
 export RESUME=True
 export PORT=2000
 export TM_PORT=8000
@@ -18,22 +15,24 @@ export DEBUG_CHALLENGE=0
 export REPETITIONS=1
 
 # TODO: change the following exports
-export TEAM_AGENT=imitation_agents/agents/action_agent.py
-export ROUTES=data/routes/routes_town01_tiny.xml
-export SCENARIOS=data/scenarios/all_towns_traffic_scenarios_autopilot.json
-export CHECKPOINT_ENDPOINT=results/autopilot_result.json
-export SAVE_PATH=datasets/autopilot
+export BASE_CODE_PATH=~/Research/Codes/Carla/carla-imitation-learning
+export TEAM_AGENT=${BASE_CODE_PATH}/imitation_agents/agents/action_agent.py
+export TEAM_CONFIG=${BASE_CODE_PATH}/checkpoints/models
+export ROUTES=${BASE_CODE_PATH}/data/routes/routes_town01_tiny.xml
+export SCENARIOS=${BASE_CODE_PATH}/data/scenarios/all_towns_traffic_scenarios_autopilot.json
+export CHECKPOINT_ENDPOINT=${BASE_CODE_PATH}/results/autopilot_result.json
+export SAVE_PATH=${BASE_CODE_PATH}/datasets/autopilot
 
 python3 ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
---scenarios=${SCENARIOS}  \
+--port=${PORT} \
+--traffic-manager-port=${TM_PORT} \
+--debug=${DEBUG_CHALLENGE} \
 --routes=${ROUTES} \
+--scenarios=${SCENARIOS}  \
 --repetitions=${REPETITIONS} \
---track=${CHALLENGE_TRACK_CODENAME} \
---checkpoint=${CHECKPOINT_ENDPOINT} \
 --agent=${TEAM_AGENT} \
 --agent-config=${TEAM_CONFIG} \
---debug=${DEBUG_CHALLENGE} \
---record=${RECORD_PATH} \
+--track=${CHALLENGE_TRACK_CODENAME} \
 --resume=${RESUME} \
---port=${PORT} \
---trafficManagerPort=${TM_PORT}
+--checkpoint=${CHECKPOINT_ENDPOINT} \
+--record=${RECORD_PATH}
